79f4d9be77892b0ad49f3b6786e4b3ce64cdd2b8
Manuel Woehrer <mani.woehrer@gmx.at>
2025-01-26 17:53:41 +0100

    some files were not comitted

--- src/phplogin/authenticate.php
+++ src/phplogin/authenticate.php
@@ -7,34 +7,35 @@ $DATABASE_PASS = 'root';
 $DATABASE_NAME = 'koeller';
 
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if ( mysqli_connect_errno() ) {
+if (mysqli_connect_errno()) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
-if ( !isset($_POST['user_name'], $_POST['password']) ) {
+if (!isset($_POST['username'], $_POST['password'])) {
 	exit('Please fill both the username and password fields!');
 }
 
-if ($stmt = $con->prepare('SELECT id, password FROM users WHERE user_name = ?')) {
-	$stmt->bind_param('s', $_POST['user_name']);
-	$stmt->execute();
-	$stmt->store_result();
+if ($userQuery = $con->prepare('SELECT id, password FROM users WHERE user_name = ?')) {
+	$userQuery->bind_param('s', $_POST['username']);
+	$userQuery->execute();
+	$userQuery->store_result();
 
-	if ($stmt->num_rows > 0) {
-		$stmt->bind_result($id, $password);
-		$stmt->fetch();
+	if ($userQuery->num_rows > 0) {
+		$userQuery->bind_result($id, $password);
+		$userQuery->fetch();
+		echo $role;
 		if (password_verify($_POST['password'], $password)) {
 			session_regenerate_id();
 			$_SESSION['loggedin'] = TRUE;
-			$_SESSION['name'] = $_POST['user_name'];
 			$_SESSION['id'] = $id;
+			$_SESSION['username'] = $_POST['username'];
 			header('Location: home.php');
 		} else {
 			echo 'Incorrect username and/or password!';
 		}
 	} else {
-		echo 'Incorrect user_name and/or password!';
+		echo 'Incorrect username and/or password!';
 	}
 
-	$stmt->close();
+	$userQuery->close();
 }
 ?>
--- src/phplogin/profile.php
+++ src/phplogin/profile.php
@@ -1,7 +1,5 @@
 <?php
-// We need to use sessions, so you should always start sessions using the below code.
 session_start();
-// If the user is not logged in redirect to the login page...
 if (!isset($_SESSION['loggedin'])) {
 	header('Location: index.html');
 	exit;
@@ -14,9 +12,7 @@ $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_
 if (mysqli_connect_errno()) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
-// We don't have the password info stored in sessions, so instead, we can get the results from the database.
 $stmt = $con->prepare('SELECT password FROM users WHERE id = ?');
-// In this case we can use the account ID to get the account info.
 $stmt->bind_param('i', $_SESSION['id']);
 $stmt->execute();
 $stmt->bind_result($password);
@@ -26,106 +22,22 @@ $stmt->close();
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<title>Profile Page</title>
-		<link href="style.css" rel="stylesheet" type="text/css">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
-	</head>
-	<body class="loggedin">
-		<nav class="navtop">
-			<div>
-				<h1>Website Title</h1>
-				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
-				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
-			</div>
-		</nav>
-		<div class="content">
-			<h2>Profile Page</h2>
-			<div>
-				<p>Your account details are below:</p>
-				<table>
-					<tr>
-						<td>Username:</td>
-						<td><?=htmlspecialchars($_SESSION['name'], ENT_QUOTES)?></td>
-					</tr>
-					<tr>
-						<td>Password:</td>
-						<td><?=htmlspecialchars($password, ENT_QUOTES)?></td>
-					</tr>
-				</table>
-			</div>
-		</div>
-	</body>
-</html>
 
-<style>
-	.navtop {
-	background-color: #2f3947;
-	height: 60px;
-	width: 100%;
-	border: 0;
-}
-.navtop div {
-	display: flex;
-	margin: 0 auto;
-	width: 1000px;
-	height: 100%;
-}
-.navtop div h1, .navtop div a {
-	display: inline-flex;
-	align-items: center;
-}
-.navtop div h1 {
-	flex: 1;
-	font-size: 24px;
-	padding: 0;
-	margin: 0;
-	color: #eaebed;
-	font-weight: normal;
-}
-.navtop div a {
-	padding: 0 20px;
-	text-decoration: none;
-	color: #c1c4c8;
-	font-weight: bold;
-}
-.navtop div a i {
-	padding: 2px 8px 0 0;
-}
-.navtop div a:hover {
-	color: #eaebed;
-}
-body.loggedin {
-	background-color: #f3f4f7;
-}
-.content {
-	width: 1000px;
-	margin: 0 auto;
-}
-.content h2 {
-	margin: 0;
-	padding: 25px 0;
-	font-size: 22px;
-	border-bottom: 1px solid #e0e0e3;
-	color: #4a536e;
-}
-.content > p, .content > div {
-	box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
-	margin: 25px 0;
-	padding: 25px;
-	background-color: #fff;
-}
-.content > p table td, .content > div table td {
-	padding: 5px;
-}
-.content > p table td:first-child, .content > div table td:first-child {
-	font-weight: bold;
-	color: #4a536e;
-	padding-right: 15px;
-}
-.content > div p {
-	padding: 5px;
-	margin: 0 0 10px 0;
-}
-</style>
+<head>
+	<?php include 'meta.php'; ?>
+</head>
+
+<body class="loggedin">
+	<?php include 'header.php'; ?>
+	<div class="container">
+		<h2>Profil</h2>
+		<div>
+			Username: <?= htmlspecialchars($_SESSION['username'], ENT_QUOTES) ?>
+		</div>
+		<div>
+			Passwort: <?= htmlspecialchars($password, ENT_QUOTES) ?>
+		</div>
+	</div>
+</body>
+
+</html>
